03da9e886cc9b50f568c8773732c010fb19fa572
Vincent <2w121q@gmail.com>
2024-03-03 19:25:55 +0800

    Arrange variables & functions

--- javascript.js
+++ javascript.js
@@ -1,66 +1,65 @@
-let game = (function() {
-
-    return { };
-})();
-
 let p1;
 let p2;
-let modal = document.querySelector(`dialog`);
-modal.showModal();
-let currentPlayer;
-let start = document.querySelector(`dialog button`);
-start.addEventListener('click', () => {
-    p1 = createPlayer(document.querySelector(`#p1`).value);
-    p2 = createPlayer(document.querySelector(`#p2`).value);
-    currentPlayer = p1.name;
-    gameboard.updateDisplay(currentPlayer);
-    modal.close();
-});
 
-let gameboard = (function() {
-    let board = [];
-    let marker = 'X';
+let game = (function() {
+    let currentPlayer;
     let tieCounter = 0;
+    let modal = document.querySelector(`dialog`);
+    let display = document.querySelector(`#display`);
     let cont = document.querySelector(`#container`);
+
+    modal.showModal();    
+    let start = document.querySelector(`dialog button`);
+    start.addEventListener('click', () => {
+        p1 = createPlayer(document.querySelector(`#p1`).value);
+        p2 = createPlayer(document.querySelector(`#p2`).value);
+        currentPlayer = p1.name;
+        gameboard.updateDisplay(currentPlayer);
+        modal.close();
+    });
 
     let restart = document.querySelector(`#container + button`);
     restart.addEventListener('click', () => {
-        board = [];
-        displayBoard();
-        marker = 'X';
+        gameboard.board = [];
+        gameboard.displayBoard();
+        gameboard.marker = 'X';
         tieCounter = 0;
         currentPlayer = p1.name;
-        updateDisplay(currentPlayer);
+        gameboard.updateDisplay(currentPlayer);
     });
-    
-    let display = document.querySelector(`#display`);
 
-    function updateDisplay(currentPlayer) {
-        display.textContent = `${currentPlayer}'s turn ( ${marker} )`;            
-    }   
-        
     cont.addEventListener('click', (event) => {
         let spot = event.target;
         let index = spot.getAttribute('id').split('t')[1];
 
-        if (board[index]);
+        if (gameboard.board[index]);
         else {
-            board[index] = marker;
+            gameboard.board[index] = gameboard.marker;
             tieCounter++;   
-            displayBoard();
+            gameboard.displayBoard();
 
-            let action = checkBoard(marker);
+            let action = gameboard.checkBoard(gameboard.marker);
             if (action === 'end') {
-                display.textContent = `${currentPlayer}(${marker}) wins!`;              
+                display.textContent = `${currentPlayer}(${gameboard.marker}) wins!`;              
             } else if (tieCounter === 9) {
                 display.textContent = `It's a TIE!`;
             } else {
                 currentPlayer = (currentPlayer == p1.name) ? p2.name : p1.name;
-                marker = (marker == 'X') ? 'O' : 'X';
-                updateDisplay(currentPlayer);                
+                gameboard.marker = (gameboard.marker == 'X') ? 'O' : 'X';
+                gameboard.updateDisplay(currentPlayer);                
             }           
         }       
     });
+    return { display, currentPlayer };
+})();
+
+let gameboard = (function() {    
+    let board = [];
+    let marker = 'X';                    
+
+    function updateDisplay(currentPlayer) {
+        game.display.textContent = `${currentPlayer}'s turn ( ${marker} )`;            
+    }       
 
     function displayBoard() {
         for (let i = 0; i < 9; i++) {
@@ -88,52 +87,10 @@ let gameboard = (function() {
         } else if (board[2] === marker && board[4] === marker && board[6] === marker) {
             return 'end';
         }
-    }
-
-    for (let i = 0; i < 9; i++) {
-        break        
-        function checkBoard(marker) {
-            if (board[0] === marker && board[1] === marker && board[2] === marker) {
-                return 'end';
-            } else if (board[3] === marker && board[4] === marker && board[5] === marker) {
-                return 'end';
-            } else if (board[6] === marker && board[7] === marker && board[8] === marker) {
-                return 'end';
-            } else if (board[0] === marker && board[3] === marker && board[6] === marker) {
-                return 'end';
-            } else if (board[1] === marker && board[4] === marker && board[7] === marker) {
-                return 'end';
-            } else if (board[2] === marker && board[5] === marker && board[8] === marker) {
-                return 'end';
-            } else if (board[0] === marker && board[4] === marker && board[8] === marker) {
-                return 'end';
-            } else if (board[2] === marker && board[4] === marker && board[6] === marker) {
-                return 'end';
-            }
-        }
-        
-        let end = check('X');
-        if (end === 'end') break;
-        else if (i === 8) {
-            console.log('draw');
-            break
-        }
-    }
-    
-    return { updateDisplay };
+    }        
+    return { updateDisplay, board, marker, displayBoard, checkBoard };
 })();
 
 function createPlayer (name) {    
     return { name };
 }
-
-// let xCount = 0;
-// for (let i = 0; i < board.length; i++) {
-//     for (let j = 0; j < row1.length; j++) {
-//         if (board[i][j] === 'x') xCount++;
-//     }
-// }
-// if (xCount === 3) {
-//     console.log('X Player wins');
-//     break
-// }
