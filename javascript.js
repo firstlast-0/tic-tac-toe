666b51b423b3f104e4b784c2c06843883ca1a424
Vincent <2w121q@gmail.com>
2024-03-03 14:51:56 +0800

    Add the restart button & rest of interface

--- javascript.js
+++ javascript.js
@@ -3,11 +3,42 @@ let game = (function() {
     return { };
 })();
 
-let gameboard = (function() {
-    let board = ['X', 'O', 'X', '', 'X'];
-    let cont = document.querySelector(`#container`);
-    let marker = 'X';
+let p1;
+let p2;
+let modal = document.querySelector(`dialog`);
+modal.showModal();
+let currentPlayer;
+let start = document.querySelector(`dialog button`);
+start.addEventListener('click', () => {
+    p1 = createPlayer(document.querySelector(`#p1`).value);
+    p2 = createPlayer(document.querySelector(`#p2`).value);
+    currentPlayer = p1.name;
+    gameboard.updateDisplay(currentPlayer);
+    modal.close();
+});
 
+let gameboard = (function() {
+    let board = [];
+    let marker = 'X';
+    let tieCounter = 0;
+    let cont = document.querySelector(`#container`);
+
+    let restart = document.querySelector(`#container + button`);
+    restart.addEventListener('click', () => {
+        board = [];
+        displayBoard();
+        marker = 'X';
+        tieCounter = 0;
+        currentPlayer = p1.name;
+        updateDisplay(currentPlayer);
+    });
+    
+    let display = document.querySelector(`#display`);
+
+    function updateDisplay(currentPlayer) {
+        display.textContent = `${currentPlayer}'s turn ( ${marker} )`;            
+    }   
+        
     cont.addEventListener('click', (event) => {
         let spot = event.target;
         let index = spot.getAttribute('id').split('t')[1];
@@ -15,36 +46,53 @@ let gameboard = (function() {
         if (board[index]);
         else {
             board[index] = marker;
-            marker = (marker == 'X') ? 'O' : 'X';
+            tieCounter++;   
             displayBoard();
+
+            let action = checkBoard(marker);
+            if (action === 'end') {
+                display.textContent = `${currentPlayer}(${marker}) wins!`;              
+            } else if (tieCounter === 9) {
+                display.textContent = `It's a TIE!`;
+            } else {
+                currentPlayer = (currentPlayer == p1.name) ? p2.name : p1.name;
+                marker = (marker == 'X') ? 'O' : 'X';
+                updateDisplay(currentPlayer);                
+            }           
         }       
     });
 
     function displayBoard() {
         for (let i = 0; i < 9; i++) {
-            if (board[i]) {
-                let spot = document.querySelector(`#spot${i}`);
-                spot.textContent = board[i]
-            }        
+            let spot = document.querySelector(`#spot${i}`);
+            spot.textContent = board[i]                
         }
     }
-    displayBoard();
+    displayBoard(); 
     
-
-    let p1 = createPlayer('user1');
-    let p2 = createPlayer('user2');    
-
-    function pick() {
-        let msg = (marker == 'X') ? 'Spot for X' : 'Spot for O';
-        let spot = prompt(msg);
-        board[spot] = marker;
-        
+    function checkBoard(marker) {
+        if (board[0] === marker && board[1] === marker && board[2] === marker) {
+            return 'end';
+        } else if (board[3] === marker && board[4] === marker && board[5] === marker) {
+            return 'end';
+        } else if (board[6] === marker && board[7] === marker && board[8] === marker) {
+            return 'end';
+        } else if (board[0] === marker && board[3] === marker && board[6] === marker) {
+            return 'end';
+        } else if (board[1] === marker && board[4] === marker && board[7] === marker) {
+            return 'end';
+        } else if (board[2] === marker && board[5] === marker && board[8] === marker) {
+            return 'end';
+        } else if (board[0] === marker && board[4] === marker && board[8] === marker) {
+            return 'end';
+        } else if (board[2] === marker && board[4] === marker && board[6] === marker) {
+            return 'end';
+        }
     }
 
     for (let i = 0; i < 9; i++) {
-        break
-        pick();
-        function check(marker) {
+        break        
+        function checkBoard(marker) {
             if (board[0] === marker && board[1] === marker && board[2] === marker) {
                 return 'end';
             } else if (board[3] === marker && board[4] === marker && board[5] === marker) {
@@ -72,7 +120,7 @@ let gameboard = (function() {
         }
     }
     
-    return { board };
+    return { updateDisplay };
 })();
 
 function createPlayer (name) {    
